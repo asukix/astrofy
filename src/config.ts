d9b3527fba6e931e1bb51e175f8ece47d264ee36
asukix <asukix@gmail.com>
2026-06-04 15:14:35 +0200

    wm

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Sengdev';
+export const SITE_TITLE = 'SENGDEV>';
 export const SITE_DESCRIPTION = 'Sengdev is my personal website and a thinking space.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
