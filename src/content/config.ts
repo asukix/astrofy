0cd2039c8710a88b920002a9882fc69fa4789222
asukix <asukix@gmail.com>
2026-06-09 09:23:56 +0200

    draft

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,12 @@
 import { z, defineCollection } from "astro:content";
 const blogSchema = z.object({
     title: z.string(),
-    description: z.string(),
+    description: z.string().optional(),
     pubDate: z.coerce.date(),
     updatedDate: z.string().optional(),
     heroImage: z.string().optional(),
     badge: z.string().optional(),
+    draft: z.boolean().optional(),
     thinkingArticle: z.string().optional(),
     tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
         message: 'tags must be unique',
