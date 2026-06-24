5cee5f2c23423fb9f082edee5195a4db4744509a
asukix <asukix@gmail.com>
2026-06-24 11:12:52 +0200

    define update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://sengdev.com',
   //integrations: [mdx(), sitemap(), tailwind()]
   integrations: [mdx(), tailwind()]
 });
